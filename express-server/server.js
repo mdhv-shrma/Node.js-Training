c6a0107d643da879e2f3ac7a24b218916513dade
mdhv-shrma <madhav.sharma@ksolves.com>
2025-03-18 20:16:20 +0530

    added dynamic routing

--- express-server/server.js
+++ express-server/server.js
@@ -1,17 +1,29 @@
 const express = require("express");
+const url = require("url");
 const app = express();
 
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the Home Page</h1>");
+  
 });
 
+app.get("/user/:name", (req,res)=>{
+  const name = req.params.name;
+  res.send(`Hello, ${name}!`);
+})
+
 app.get("/about", (req, res) => {
-  res.send("<h1>About Us</h1><p>We are learning Express.js!</p>");
+  res.send("<h1>About Us</h1><p>about page</p>");
 });
 
 app.get("/contact", (req, res) => {
   res.send("<h1>Contact Us</h1><p>Email: contact@example.com</p>");
+});
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
 });
 
 
